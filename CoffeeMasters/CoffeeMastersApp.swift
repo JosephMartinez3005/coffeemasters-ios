2adbb0b05121f561222fa81a0fa1bde6c697ba3a
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2022-08-23 09:43:21 -0700

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Joseph Martinez on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
